108673558a8af59cf0c02da9a49510e8578a0969
ZHIYUN WU <wzyunzx@163.com>
2016-04-18 08:56:32 +0100

    Change database for mongodb

--- models/coffees.js
+++ models/coffees.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
-
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } },
+    user: 'wzyzcz', pass: 'wzyzcz' };
+var mongodbUri = 'mongodb://root:wzyzcz@ds011291.mlab.com:11291/heroku_7lnp807'
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 var CoffeeSchema = new mongoose.Schema({
     name: String,
     shop: String,
